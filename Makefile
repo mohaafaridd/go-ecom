fbc74f235e462c61beaab8fc31d915bc5a665ea1
Mohammed Farid <dev.mohammedfarid@gmail.com>
2025-04-12 03:02:40 +0200

    feat(migration): :sparkles: add database migration support and user, product, order, order items tables
    - Updated Makefile to include migration commands.
    - Implemented migration logic in cmd/migrate/main.go.
    - Created SQL migration files for users, products, orders, and order_items tables with up and down scripts.

--- Makefile
+++ Makefile
@@ -1,6 +1,15 @@
 build:
 	@go build -o bin/ecom cmd/main.go
 test:
-	@go test -b ./...
+	@go test -v ./...
 run: build
 	@./bin/ecom
+
+migration:
+	@migrate create -ext sql -dir cmd/migrate/migrations $(filter-out $@,$(MAKECMDGOALS))
+
+migrate-up:
+	@go run cmd/migrate/main.go up
+
+migrate-down:
+	@go run cmd/migrate/main.go down
---
+++ cmd/migrate/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	mysqlConfig "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"mohaafaridd.dev/ecom/config"
+	"mohaafaridd.dev/ecom/db"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysqlConfig.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://cmd/migrate/migrations", "mysql", driver)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[(len(os.Args) - 1)]
+	if cmd == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	} else {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+}
